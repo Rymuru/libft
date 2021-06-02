245498e7cfb14af05ae4c740c2b59abe710a1f77
Baptiste COENON <bcoenon@e2r8p4.42.fr>
2021-06-02 11:19:49 +0200

    part 1 finalR

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 17:06:54 by bcoenon           #+#    #+#             */
-/*   Updated: 2021/06/02 11:06:28 by bcoenon          ###   ########.fr       */
+/*   Updated: 2021/06/02 11:18:10 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ char	*ft_strnstr(const char *h, const char *n, size_t len)
 	size_t	j;
 
 	if (!n[0])
-		return ((char*)h);
+		return ((char *)h);
 	i = 0;
 	while (h[i] && i < len)
 	{
 		j = 0;
-		while (h[i + j] && n[j] &&
-				i + j < len && h[i + j] == n[j])
+		while (h[i + j] && n[j]
+			&& i + j < len && h[i + j] == n[j])
 			j++;
 		if (!n[j])
-			return ((char*)(h + i));
+			return ((char *)(h + i));
 		i++;
 	}
 	return (NULL);
