36cdaeee1193ee039bad3fcfe208862247eccd75
Baptiste COENON <bcoenon@e3r7p18.42.fr>
2021-06-11 17:01:36 +0200

    corrct strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 14:13:29 by bcoenon           #+#    #+#             */
-/*   Updated: 2021/05/28 14:52:58 by bcoenon          ###   ########.fr       */
+/*   Updated: 2021/06/11 16:58:27 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
+	char	*str;
 
-	i = ft_strlen(s);
-	if (s[i] == c)
-		return ((char *)s + i);
-	i--;
-	while (i-- > 0 && s[i])
+	str = (char *)s;
+	i = 0;
+	while (str[i])
+		i++;
+	if ((char)c == 0)
+		return (str + i);
+	while (i-- > 0)
 	{
-		if (s[i] == c)
-			return ((char *)s + i);
+		if (str[i] == (char)c)
+			return (str + i);
 	}
-	return (NULL);
+	return (0);
 }
